d42c435f992650e3c077329faf5c48458d39ad74
Sam <Sam@164.76.17.179>
2019-11-26 19:02:43 -0500

    a player object is created for each instance of odraft

--- src/baseball/Main.java
+++ src/baseball/Main.java
@@ -19,6 +19,7 @@ public class Main {
 		// System.out.println();
 		// League.printPitcher();
 
+	
 		while (true) {
 			System.out.println("Please choose an option by typing the corresponding number:");
 			System.out.println("1:  ODRAFT");
