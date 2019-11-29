4fae7c832cb52416b422c1ab061109a05ecb0c7b
Jeremy Moore <jeremy00moore@gmail.com>
2019-11-29 18:32:19 -0500

    quick bug fix in main

--- src/baseball/Main.java
+++ src/baseball/Main.java
@@ -13,92 +13,95 @@ public class Main {
 		String[] command = null;
 
 		try (Scanner scanner = new Scanner(System.in)) {
+
 			do {
-				System.out.print("Enter command: ");
-				input = scanner.nextLine();
-				command = input.split("(?<!,)[ \"]+");
 
-				if (command == null || command.length == 0) {
-					throw new IllegalArgumentException();
+				try {
+
+					System.out.print("Enter command: ");
+					input = scanner.nextLine();
+					command = input.split("(?<!,)[ \"]+");
+
+					if (command == null || command.length == 0) {
+						throw new IllegalArgumentException();
+					}
+
+					switch (command[0].toLowerCase()) {
+					case "odraft":
+						if (command.length < 3) {
+							throw new IllegalArgumentException();
+						}
+						FunctionsW.odraft(league, command[1], command[2].trim());
+						break;
+
+					case "idraft":
+						if (command.length < 2) {
+							throw new IllegalArgumentException();
+						}
+						FunctionsW.idraft(league, command[1]);
+						break;
+
+					case "overall":
+						String position = "";
+						if (command.length > 1) {
+							position = command[1];
+						}
+						FunctionsJ.overall(league.players, league.memberA, position);
+						break;
+
+					case "poverall":
+						FunctionsJ.poverall(league.pitchers, league.memberA);
+						break;
+
+					case "team":
+						if (command.length < 2) {
+							throw new IllegalArgumentException();
+						}
+						LeagueMember memberTeam = league.getMemberFromName(command[1]);
+						FunctionsP.team(memberTeam);
+						break;
+					case "stars":
+						if (command.length < 2) {
+							throw new IllegalArgumentException();
+						}
+						LeagueMember memberStars = league.getMemberFromName(command[1]);
+						FunctionsP.team(memberStars);
+						break;
+					case "save":
+						if (command.length < 2) {
+							throw new IllegalArgumentException();
+						}
+						FunctionsP.save(league);
+						break;
+					case "quit":
+						break;
+					case "restore":
+						if (command.length < 2) {
+							throw new IllegalArgumentException();
+						}
+						FunctionsP.restore(league);
+						break;
+					case "evalfun":
+						if (command.length < 2) {
+							throw new IllegalArgumentException();
+						}
+						FunctionsJ.evalfun(league.players, command[1]);
+						break;
+					case "pevalfun":
+						if (command.length < 2) {
+							throw new IllegalArgumentException();
+						}
+						FunctionsJ.pevalfun(league.pitchers, command[1]);
+						break;
+					default:
+						System.out.println("ERROR: Command '" + command[0] + "' not recognized.");
+					}
+
+				} catch (IllegalArgumentException e) {
+					System.out.println("ERROR: Wrong number of arguments provided.");
 				}
-
-				switch (command[0].toLowerCase()) {
-				case "odraft":
-					if (command.length < 3) {
-						throw new IllegalArgumentException();
-					}
-					FunctionsW.odraft(league, command[1], command[2].trim());
-					break;
-
-				case "idraft":
-					if (command.length < 2) {
-						throw new IllegalArgumentException();
-					}
-					FunctionsW.idraft(league, command[1]);
-					break;
-
-				case "overall":
-					String position = "";
-					if (command.length > 1) {
-						position = command[1];
-					}
-					FunctionsJ.overall(league.players, league.memberA, position);
-					break;
-
-				case "poverall":
-					FunctionsJ.poverall(league.pitchers, league.memberA);
-					break;
-
-				case "team":
-					if (command.length < 2) {
-						throw new IllegalArgumentException();
-					}
-					LeagueMember memberTeam = league.getMemberFromName(command[1]);
-					FunctionsP.team(memberTeam);
-					break;
-				case "stars":
-					if (command.length < 2) {
-						throw new IllegalArgumentException();
-					}
-					LeagueMember memberStars = league.getMemberFromName(command[1]);
-					FunctionsP.team(memberStars);
-					break;
-				case "save":
-					if (command.length < 2) {
-						throw new IllegalArgumentException();
-					}
-					FunctionsP.save(league);
-					break;
-				case "quit":
-					break;
-				case "restore":
-					if (command.length < 2) {
-						throw new IllegalArgumentException();
-					}
-					FunctionsP.restore(league);
-					break;
-				case "evalfun":
-					if (command.length < 2) {
-						throw new IllegalArgumentException();
-					}
-					FunctionsJ.evalfun(league.players, command[1]);
-					break;
-				case "pevalfun":
-					if (command.length < 2) {
-						throw new IllegalArgumentException();
-					}
-					FunctionsJ.pevalfun(league.pitchers, command[1]);
-					break;
-				default:
-					System.out.println("ERROR: Command '" + command[0] + "' not recognized.");
-				}
-
 			} while (!"quit".equals(command[0]));
-			
-		} catch (IllegalArgumentException e) {
-			System.out.println("ERROR: Wrong number of arguments provided.");
 		}
-
 		System.out.println("\nGoodbye!");
 	}
 }
