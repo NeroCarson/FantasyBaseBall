268f08ab14635d4280bc79031161f547520e08d9
Sam <Sam@192.168.0.17>
2019-11-30 20:51:41 -0500

    continued working on the odraft test

--- src/baseball/TestOdraft.java
+++ src/baseball/TestOdraft.java
@@ -11,34 +11,38 @@ import org.junit.Test;
 
 import models.League;
 import models.LeagueMember;
-import models.Person;
 import models.Pitcher;
 import models.Player;
 
-public class TestOdraft {	
-	League l = new League();	
-	ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+public class TestOdraft {
 
+	ArrayList<Player> players;
+	LeagueMember memberA;
+	ByteArrayOutputStream output;
+	
+	League testLeague = loadData.openFile("stats.csv", "stats_pitcher.csv");
+	
 	@Before
-	void before() {		
-		System.setOut(new PrintStream(outContent));		
-		
-		Player player1 = new Player("Yelich, C","MIL","RF");
-		Player player2 = new Player("Marte, K", "ARI","CF");
-		l.players.add(player1);
-		l.players.add(player2);
+	public void setUp() {
+		output = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(output));
 
-		Pitcher pitcher1 = new Pitcher("Ryu, H", "LAD");
-		Pitcher pitcher2 = new Pitcher("DeGrome, J", "NYM");
-		
-		l.pitchers.add(pitcher1);
-		l.pitchers.add(pitcher2);
+		players = new ArrayList<>();
+		Player player1 = new Player("Yelich, C", "MIL", "RF");
+		memberA = new LeagueMember("A");
+		memberA.team.ss = player1;
+		players.add(player1);
+
 	}
+	
+	// failing???
+
 	@Test
-	public void odraft() throws Throwable{
-		FunctionsW.odraft()
-		//when 
-		//assertEquals(expected, result);
+	public void odraft() {
+		FunctionsW.odraft(testLeague, "Yelich, C", "A");
+		String expected = "You've recruited Yelich, C for the position of RF\n";
+		String actual = output.toString()+"";
+		assertEquals(expected, actual);
 	}
 
 }
